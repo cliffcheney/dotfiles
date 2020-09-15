b6fe77bc8bef3a7a5ec077122bd3de95ee29277e
Cliff Cheney <cliff@cliffcheney.com>
2020-09-15 02:19:11 +0200

    Relinking scripts works. moves old to _old files.

--- dotsymlink.sh
+++ dotsymlink.sh
@@ -7,5 +7,15 @@
 array=("bash_profile" "bashrc" "config" "gitconfig" "tmux.conf" "vim" "vimrc" "zshrc")  
 
 for t in ${array[@]}; do
-	 ln -s ~/dotfiles/$t ~/.$t 
-  done
+	cd ~/
+	mv .$t .$t"_old"
+	ln -s dotfiles/$t .$t 
+done
+
+# Remove vim bundles and then re-cole Vundle and then run Vundle plugininstall
+
+  rm -Rf ~/.vim/bundle/*
+
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+  vim +PluginInstall +qall!
