27e8ca3cdacad6342136c989e86ebd70cb191f32
Cliff Cheney <cliff@cliffcheney.com>
2020-09-15 03:20:46 +0200

    Moved vim swap loction

--- dotsymlink.sh
+++ dotsymlink.sh
@@ -3,8 +3,9 @@
 # Script to make symlink into correct places. 
 # No deleting
 # Just brute ln -s in the hopes that erros won't kill it
+#
 
-array=("bash_profile" "bashrc" "config" "gitconfig" "tmux.conf" "vimrc" "zshrc")  
+array=('bash_profile' 'bashrc' 'config' 'gitconfig' 'tmux.conf' 'vimrc' 'zshrc')  
 
 for t in ${array[@]}; do
 	cd ~/
@@ -13,7 +14,8 @@ for t in ${array[@]}; do
 done
 
 # Remove vim bundles and then re-cole Vundle and then run Vundle plugininstall
-  mkdir ~/.vim/bundle/
+  mkdir -p ~/.vim/bundle/
+  mkdir ~/.vim/swapfiles/
   rm -Rf ~/.vim/bundle/Vundle.vim
 
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
