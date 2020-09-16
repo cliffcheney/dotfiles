5cfb239b3cf43b0ee686e12767adbc93d4d96d84
Cliff Cheney <cliff@cliffcheney.com>
2020-09-16 17:13:08 +0200

    aliases for ls -la and tmuxstart.sh

--- bashrc
+++ bashrc
@@ -1,3 +1,6 @@
 export PATH=~/bin:$PATH
 
 set -o vi
+
+alias ll='ls -la'
+alias tms='tmuxstart.sh'
--- dotsymlink.sh
+++ dotsymlink.sh
@@ -24,5 +24,3 @@ done
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
   vim +PluginInstall +qall!
-
-
