f4ac896f9055b90468cf1c9cea9867ca78d168d6
Cliff Cheney <cliff@cliffcheney.com>
2021-01-08 22:17:33 +0000

    working on getting the docker setup scripted

--- cli50symlink.sh
+++ cli50symlink.sh
@@ -3,7 +3,7 @@
 # Script to make symlink into correct places. No deleting
 # Just brute ln -s in the hopes that errors won't kill it
 
-array=( 'bashrc' 'config' 'gitconfig' 'vimrc' )
+array=( 'bashrc' 'config' 'gitconfig' 'vimrc' 'tmux.conf' )
 
 for t in ${array[@]}; do
 	cd /home/ubuntu/
@@ -12,18 +12,16 @@ for t in ${array[@]}; do
 done
 
 # Remove vim bundles and then re-cole Vundle and then run Vundle plugininstall
-    mkdir -p /home/ubuntu/.vim/bundle/
-    mkdir -p /home/ubuntu/.vim/swapfiles/
+    mkdir -p /home/ubuntu/workspace/.vim/bundle/
+    mkdir -p /home/ubuntu/workspace/.vim/swapfiles/
+    ln -s /home/ubuntu/workspace/.vim /home/ubuntu/.vim
     ln -Fs /home/ubuntu/dotfiles/vim_colors /home/ubuntu/.vim/colors
     rm -Rf /home/ubuntu/.vim/bundle/Vundle.vim
 
   git clone https://github.com/VundleVim/Vundle.vim.git /home/ubuntu/.vim/bundle/Vundle.vim
 
-  vim +PluginClean
+  vim +PluginClean +qall!
   vim +PluginInstall +qall!
-
-
-sudo apt update
 
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:lazygit-team/release
