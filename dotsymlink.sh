c17dcb41d3836204b07037689bf2cf0969720c2f
Cliff Cheney <cliff@cliffcheney.com>
2021-01-12 22:31:14 +0000

    garabage cleanup

--- dotsymlink.sh
+++ dotsymlink.sh
@@ -3,12 +3,12 @@
 # Script to make symlink into correct places. No deleting
 # Just brute ln -s in the hopes that errors won't kill it
 
-array=('bash_profile' 'bashrc' 'config' 'gitconfig' 'tmux.conf' 'vimrc' 'zshrc')  
+array=('bash_profile' 'bashrc' 'config' 'gitconfig' 'tmux.conf' 'vimrc' 'zshrc')
 
 for t in ${array[@]}; do
 	cd ~/
 	mv .$t .$t"_old"
-	ln -s dotfiles/$t .$t 
+	ln -s dotfiles/$t .$t
 done
 
 # link tmuxstart into bin
@@ -18,12 +18,14 @@ done
 # Remove vim bundles and then re-cole Vundle and then run Vundle plugininstall
     mkdir -p ~/.vim/bundle/
     mkdir -p ~/.vim/swapfiles/
+    rm ~/.vim/colors
+
     ln -Fs ~/dotfiles/vim_colors ~/.vim/colors
     rm -Rf ~/.vim/bundle/Vundle.vim
 
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-  vim +PluginClean 
+  vim +PluginClean
   vim +PluginInstall +qall!
 
   # if Macos then install alacritty service workflow
