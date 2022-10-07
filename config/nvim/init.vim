9b0ca2d66c144889e540fe54f23d1d7d4b4b0d45
Cliff Cheney <cliff@cliffcheney.com>
2022-10-07 21:15:56 +0200

    cleaned out config dir and setup nvmi to install vim-plug

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -1,3 +1,9 @@
-    set runtimepath^=~/.vim runtimepath+=~/.vim/after
-    let &packpath = &runtimepath
-    source ~/.vimrc
+
+" auto-install vim-plug
+if empty(glob('~/.config/nvim/autoload/plug.vim'))
+  silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall | source $MYVIMRC
+endif
+
+source ~/.vimrc
--- dotsymlink.sh
+++ dotsymlink.sh
@@ -3,7 +3,7 @@
 # Script to make symlink into correct places. No deleting
 # Just brute ln -s in the hopes that errors won't kill it
 
-array=('bash_profile' 'bashrc' 'config' 'gitconfig' 'tmux.conf' 'vimrc' 'zshrc')
+array=('bash_profile' 'bashrc' 'config/nvim' 'gitconfig' 'tmux.conf' 'vim' 'vimrc' 'zshrc')
 
 for t in ${array[@]}; do
 	cd ~/
@@ -14,21 +14,3 @@ done
 # link tmuxstart into bin
     mkdir -p ~/bin
     ln -s ~/dotfiles/tmuxstart.sh ~/bin/tmuxstart.sh
-
-# Remove vim bundles and then reinstall Vundle and then run Vundle plugininstall
-    mkdir -p ~/.vim/bundle/
-    mkdir -p ~/.vim/swapfiles/
-    rm ~/.vim/colors
-
-    ln -Fs ~/dotfiles/vim_colors ~/.vim/colors
-    rm -Rf ~/.vim/bundle/Vundle.vim
-
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-  vim +PluginClean +qall!
-  vim +PluginInstall +qall!
-
-  # if Macos then install alacritty service workflow
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        ln -s ~/dotfiles/openAlacritty.workflow ~/Library/Services/
-    fi
