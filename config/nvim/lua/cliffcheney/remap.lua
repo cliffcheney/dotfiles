08676ff1ef299808034932da3f00e85fc0f6c33f
cliffcheney <cliffcheney@users.noreply.github.com>
2023-02-07 18:53:27 +0200

    kill q in neovim

--- config/nvim/lua/cliffcheney/remap.lua
+++ config/nvim/lua/cliffcheney/remap.lua
@@ -32,6 +32,7 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
